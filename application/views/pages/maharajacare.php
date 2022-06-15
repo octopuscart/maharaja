85b7e7c2153d4a78bd61744edb83742f4e6abb8e
octopuscart <octopuscartltd@gmail.com>
2022-06-15 14:30:50 +0530

    add files

--- application/views/pages/maharajacare.php
+++ application/views/pages/maharajacare.php
@@ -24,51 +24,34 @@ $this->load->view('layout/header');
 <div class="contact-us-page-area" style="padding: 10px;">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="">
-                <div class="contact-us-left" > 
-                    <img src="<?php echo base_url(); ?>assets/images/maharajacareleaflate.jpeg">
+               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="">
+                <div class="col-md-6">
+                    <img src="<?php echo base_url(); ?>assets/images/maharajacare.png" style="height: 200px">
+                   
                 </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="">
-                <img src="<?php echo base_url(); ?>assets/images/maharajacare.png" style="height: 200px">
-                <p style="font-size: 18px;line-height: initial;">
-                    An unique  and exclusive health care consultation services offered by the best in town health centers located in Mongkok and Causeway bay.
-                    Providing VIP(Maharaja) services to all our customers at an exceptionally best prices. Health Packages vary from Basic, intermediate, targeted and premium. 
-                </p>
-                <ul>
-                    <?php
-                    $checkuplist = [
-                        "Standard Health Package",
-                        "Priority Health Check Plan",
-                        "Annual Health Check Check Plan",
-                        "Royal Comprehensive Health Check Plan",
-                        "Premium Executive Health Check Plan",
-                        "Executive Cardio Health Package",
-                        "Women selective Health Check Plan",
-                        "Heart Health Screening Plan",
-                        "Premium Health Check Plan",
-                        "Comprehensive DNA Test (on 20 categories)"
-                    ];
-                    foreach ($checkuplist as $key => $value) {
-                        ?>
-                        <li>
-                           <?php
-                           echo $value;
-                           ?>
-                        </li>
-                        <?php
-                    }
-                    ?>
-
-                </ul>
-                <hr/>
-                <h3><a href="https://wa.me/85261428189" target="_blank"><i class="fa fa-whatsapp"></i> +(852) 6142 8189</a></h3>
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        <a href="https://bit.ly/3AXYrcW" target="_blank" name="sendmessage" value="sendmessage" class="btn-send-message">Book Now</a>
+                <div class="col-md-6">
+                     <p style="font-size: 18px;line-height: initial;">
+                        An unique  and exclusive health care consultation services offered by the best in town health centers located in Mongkok and Causeway bay.
+                        Providing VIP(Maharaja) services to all our customers at an exceptionally best prices. Health Packages vary from Basic, intermediate, targeted and premium. 
+                    </p>
+                      <hr/>
+                    <h3><a href="https://wa.me/85261428189" target="_blank"><i class="fa fa-whatsapp"></i> +(852) 6142 8189</a></h3>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <a href="https://bit.ly/3AXYrcW" target="_blank" name="sendmessage" value="sendmessage" class="btn-send-message">Book Now</a>
+                        </div>
                     </div>
+                   
+                </div>  
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="">
+                <div class="contact-us-left" > 
+                    <!--<img src="<?php echo base_url(); ?>assets/images/maharajacareleaflate.jpeg">\-->
+                    <iframe src="<?php echo base_url(); ?>assets/block/Health-Plans-JUNE2022.pdf" width="100%" height="800px"></iframe>
+
                 </div>
             </div>
+         
         </div>
 
 
